7f3a00c925e94a6b2fd68ef94463ba9f6fe4b0cb
Timothy Small <smalls.online@icloud.com>
2021-10-27 10:34:47 -0400

    Initial commit
    - This is the initial commit of the repo. Original repo was located here
      at: https://github.com/Smalls1652/SmallsOnline.Subnetting.

---
+++ README.md
@@ -0,0 +1,5 @@
+# `SmallsOnline.Subnetting.Pwsh`
+
+_**A C#-based subnet calculator for PowerShell.**_
+
+This repository is for a C# class library to perform subnetting of IPv4 address spaces. It is currently a work-in-progress.
---
+++ src/SmallsOnline.Subnetting.Pwsh/cmdlets/NewSubnet.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Management.Automation;
+using System.Net;
+
+namespace SmallsOnline.Subnetting.Pwsh.Cmdlets
+{
+    using Lib.Models;
+
+    [Cmdlet(VerbsCommon.New, "Subnet")]
+    public class NewSubnet : PSCmdlet
+    {
+        [Parameter(Position = 0, Mandatory = true, ParameterSetName = "Default")]
+        [ValidateNotNullOrEmpty()]
+        public IPAddress IPAddress
+        {
+            get => ipAddress;
+            set => ipAddress = value;
+        }
+
+        [Parameter(Position = 1, ParameterSetName = "Default")]
+        [ValidateNotNullOrEmpty()]
+        public double CidrNotation
+        {
+            get => cidrNotation;
+            set => cidrNotation = value;
+        }
+
+        [Parameter(Position = 0, Mandatory = true, ParameterSetName = "SubnetString")]
+        [ValidateNotNullOrEmpty()]
+        public string NetworkString
+        {
+            get => networkString;
+            set => networkString = value;
+        }
+
+        private IPAddress ipAddress;
+        private double cidrNotation = 24;
+        private string networkString;
+
+        protected override void BeginProcessing()
+        {
+            base.BeginProcessing();
+        }
+
+        protected override void ProcessRecord()
+        {
+            IPv4Subnet subnetItem = ParameterSetName switch
+            {
+                "SubnetString" => new(networkString),
+                _ => new(ipAddress, cidrNotation)
+            };
+
+            WriteObject(subnetItem);
+        }
+
+        protected override void EndProcessing()
+        {
+            base.EndProcessing();
+        }
+    }
+}
